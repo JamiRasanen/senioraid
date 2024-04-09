04e662dea93be7053fb031ca0039e05e2937643d
TheBamboopillow <Timo.Holopainen@edu.savonia.fi>
2024-04-09 14:44:09 +0300

    Juttu5

--- src/videoCall.js
+++ src/videoCall.js
@@ -313,11 +313,11 @@ const VideoCall = () => {
     // Function to generate a shorter ID
     const generateShortId = () => {
         // This is just a sample implementation. You can use any method to generate a shorter ID.
-        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let id = '12345';
-        // for (let i = 0; i < 10; i++) {
-        //     id += chars.charAt(Math.floor(Math.random() * chars.length));
-        // }
+        const chars = '0123456789';
+        let id = '';
+        for (let i = 0; i < 5; i++) {
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
         return id;
     };
 
